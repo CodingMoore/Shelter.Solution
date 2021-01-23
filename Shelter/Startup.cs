2c7ada8d1559cef2cec010861be43fda615fa81e
Randel M <Randel.C.Moore@gmail.com>
2021-01-23 10:32:29 -0800

    fix bug where UseMySql wasn't recognized in Startup.cs

--- Shelter/Startup.cs
+++ Shelter/Startup.cs
@@ -1,10 +1,10 @@
-﻿using Shelter.Models;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shelter.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Shelter
 {
