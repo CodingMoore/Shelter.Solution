ecd3e70bb915b779e8fcdb012130a162aade2df4
Randel M <Randel.C.Moore@gmail.com>
2021-01-23 13:34:47 -0800

    add entity framework and pomelo framework packages

--- Shelter/Models/ShelterContext.cs
+++ Shelter/Models/ShelterContext.cs
@@ -16,26 +16,34 @@ namespace Shelter.Models
         {
             builder.Entity<Cat>()
                 .HasData(
-                    new Cat { CatId = 1, CatName = Alphred, CatAge = 6, CatBreed = American Shorthair, CatSex = Female},
-                    new Cat { CatId = 2, CatName = Unfried, CatAge = 7, CatBreed = Bombay, CatSex = Male},
-                    new Cat { CatId = 3, CatName = Hans, CatAge = 8, CatBreed = Cyprus, CatSex = Female},
-                    new Cat { CatId = 4, CatName = Gilbert, CatAge = 9, CatBreed = Devon Rex, CatSex = Male},
-                    new Cat { CatId = 5, CatName = Heathrow, CatAge = 10, CatBreed = Egyptian Mau, CatSex = Female}
+                    new Cat { CatId = 1, CatName = "Alphred", CatAge = 6, CatBreed = "American Shorthair", CatSex = "Female"},
+                    new Cat { CatId = 2, CatName = "Unfried", CatAge = 7, CatBreed = "Bombay", CatSex = "Male"},
+                    new Cat { CatId = 3, CatName = "Hans", CatAge = 8, CatBreed = "Cyprus", CatSex = "Female"},
+                    new Cat { CatId = 4, CatName = "Gilbert", CatAge = 9, CatBreed = "Devon Rex", CatSex = "Male"},
+                    new Cat { CatId = 5, CatName = "Heathrow", CatAge = 10, CatBreed = "Egyptian Mau", CatSex = "Female"}
                 );
-        }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.Entity<Dog>()
+                builder.Entity<Dog>()
                 .HasData(
-                    new Dog { DogId = 1, DogName = Higgins, DogAge = 6, DogBreed = Akita, DogSex = Male},
-                    new Dog { DogId = 2, DogName = Basil, DogAge = 7, DogBreed = Belgian Shepherd, DogSex = Female},
-                    new Dog { DogId = 3, DogName = Oliver, DogAge = 8, DogBreed = Chilean Terrier, DogSex = Male},
-                    new Dog { DogId = 4, DogName = Hammond, DogAge = 9, DogBreed = Dingo, DogSex = Female},
-                    new Dog { DogId = 5, DogName = Sigfried, DogAge = 10, DogBreed = Estonian Hound, DogSex = Male}
-
+                    new Dog { DogId = 1, DogName = "Higgins", DogAge = 6, DogBreed = "Akita", DogSex = "Male"},
+                    new Dog { DogId = 2, DogName = "Basil", DogAge = 7, DogBreed = "Belgian Shepherd", DogSex = "Female"},
+                    new Dog { DogId = 3, DogName = "Oliver", DogAge = 8, DogBreed = "Chilean Terrier", DogSex = "Male"},
+                    new Dog { DogId = 4, DogName = "Hammond", DogAge = 9, DogBreed = "Dingo", DogSex = "Female"},
+                    new Dog { DogId = 5, DogName = "Sigfried", DogAge = 10, DogBreed = "Estonian Hound", DogSex = "Male"}
                 );
         }
+
 
     }
 }
+        // protected override void OnModelCreating(ModelBuilder builder)
+        // {
+        //     builder.Entity<Dog>()
+        //         .HasData(
+        //             new Dog { DogId = 1, DogName = "Higgins", DogAge = 6, DogBreed = "Akita", DogSex = "Male"},
+        //             new Dog { DogId = 2, DogName = "Basil", DogAge = 7, DogBreed = "Belgian Shepherd", DogSex = "Female"},
+        //             new Dog { DogId = 3, DogName = "Oliver", DogAge = 8, DogBreed = "Chilean Terrier", DogSex = "Male"},
+        //             new Dog { DogId = 4, DogName = "Hammond", DogAge = 9, DogBreed = "Dingo", DogSex = "Female"},
+        //             new Dog { DogId = 5, DogName = "Sigfried", DogAge = 10, DogBreed = "Estonian Hound", DogSex = "Male"}
+
+        //         );
+        // }
