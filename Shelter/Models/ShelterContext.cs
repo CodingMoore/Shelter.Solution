87f2a0c8843e897ebf1d2b3e51b04b3b578ea33c
Randel M <Randel.C.Moore@gmail.com>
2021-01-23 10:40:09 -0800

    add database seed data

--- Shelter/Models/ShelterContext.cs
+++ Shelter/Models/ShelterContext.cs
@@ -11,5 +11,31 @@ namespace Shelter.Models
 
         public DbSet<Cat> Cats { get; set; }
         public DbSet<Dog> Dogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Cat>()
+                .HasData(
+                    new Cat { CatId = 1, CatName = , CatAge = , CatBreed = , CatSex = },
+                    new Cat { CatId = 2, CatName = , CatAge = , CatBreed = , CatSex = },
+                    new Cat { CatId = 3, CatName = , CatAge = , CatBreed = , CatSex = },
+                    new Cat { CatId = 4, CatName = , CatAge = , CatBreed = , CatSex = },
+                    new Cat { CatId = 5, CatName = , CatAge = , CatBreed = , CatSex = }
+                );
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Dog>()
+                .HasData(
+                    new Dog { DogId = 1, DogName = , DogAge = , DogBreed = , DogSex = },
+                    new Dog { DogId = 2, DogName = , DogAge = , DogBreed = , DogSex = },
+                    new Dog { DogId = 3, DogName = , DogAge = , DogBreed = , DogSex = },
+                    new Dog { DogId = 4, DogName = , DogAge = , DogBreed = , DogSex = },
+                    new Dog { DogId = 5, DogName = , DogAge = , CatBreed = , CatSex = }
+
+                );
+        }
+
     }
 }
